31198874336b1a2e430a9135f73af576c773e3de
ali123321123 <ali2016zedan@gmail.com>
2021-10-29 14:26:57 +0200

    render recipe list

--- shopingList/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopingList/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes:Recipe[] = [
+  new Recipe('A Test recipe','This is simply a test',
+  'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=700,636'),
+  new Recipe('A Test recipe','This is simply a test',
+  'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=700,636')
+];
   constructor() { }
 
   ngOnInit(): void {
